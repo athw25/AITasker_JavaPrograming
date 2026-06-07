8d4e1553e30ede5833e180878e226a4d0ef5338b
hoanganhthu-a <htat.athw11@gmail.com>
2026-06-07 12:33:27 +0700

    Setup SQL Server and JPA

--- aitasker/src/main/java/com/aitasker/user/repository/UserRepository.java
+++ aitasker/src/main/java/com/aitasker/user/repository/UserRepository.java
@@ -1,1 +1,8 @@
-// UserRepository.java
+package com.aitasker.repository;
+
+import com.aitasker.entity.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface UserRepository
+        extends JpaRepository<User, Long> {
+}
